757f1f2ef7e53d8f12d27ad6baeed05f598195f9
池边的树 <954127715@qq.com>
2019-07-21 20:41:11 +0800

    Add files via upload

---
+++ 155最小栈.py
@@ -0,0 +1,60 @@
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+'''
+方法一：直接在栈中遍历最小值，但题中要求使用常数时间返回最小值
+'''
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        min_value = float('inf')
+        for x in self.stack:
+            if x < min_value:
+                min_value = x
+        return min_value
+
+'''
+方法二：使用辅助栈
+'''
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        if not self.min_stack or x <= self.min_stack[-1]:
+            self.min_stack.append(x)
+
+    def pop(self) -> None:
+        if self.stack.pop() == self.min_stack[-1]:
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
