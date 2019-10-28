c0c7a63127d95cba80090d84a5e97fce606b444c
池边的树 <954127715@qq.com>
2019-10-28 18:02:20 +0800

    Add files via upload

---
+++ 443压缩字符串.py
@@ -0,0 +1,19 @@
+'''
+双指针
+空间复杂度O(1)，时间复杂度O(n)
+'''
+
+
+class Solution(object):
+    def compress(self, chars):
+        anchor = write = 0
+        for read, c in enumerate(chars):
+            if read + 1 == len(chars) or chars[read] != chars[read + 1]:
+                chars[write] = chars[anchor]
+                write += 1
+                if read > anchor:
+                    for digit in str(read - anchor + 1):
+                        chars[write] = digit
+                        write += 1
+                anchor = read + 1
+        return write
